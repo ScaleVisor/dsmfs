93e5097c2aba98865836f9f981ea4250de13cb61
brisco007 <45459074+brisco007@users.noreply.github.com>
2021-08-20 10:53:54 +0100

    removing write_protect

--- unmap.c
+++ unmap.c
@@ -43,13 +43,13 @@ static int dsm_page_unmap_one(struct page *page, struct vm_area_struct *vma,
 	pte_unmap_unlock(pte, ptl);
 
 	if (ret) {
-		if(!clear_read){
-			mmu_notifier_write_protect_page(mm, address);		
+		//if(!clear_read){
+		//	mmu_notifier_write_protect_page(mm, address);		
 			//printk(KERN_DEBUG "WRITE PROTECT COUNT %ld", ++counter);
-		}else{
+		//}else{
 			mmu_notifier_invalidate_page(mm, address);
 			//printk(KERN_DEBUG "ZAP COUNT %ld", ++zapcounter);
-		}
+		//}
 	}
 out:
 	return SWAP_AGAIN;
